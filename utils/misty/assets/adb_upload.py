7e651442b9bbfb9f4f7c47b028da84bd35d16f2c
acushner <sweettuse@gmail.com>
2019-10-22 20:02:15 -0400

    cleanup

--- utils/misty/assets/adb_upload.py
+++ utils/misty/assets/adb_upload.py
@@ -7,28 +7,27 @@ __author__ = 'acushner'
 out = '/sdcard/audio/Audio'
 
 
-def upload_audio_to_misty(d):
+def upload_audio_to_misty(d, ip='', port=5555):
     """
     misty has a 3mb upload limit on audio files. this works around that.
 
     give it a directory where your audio files are organized into folders,
     and this will automatically upload them to misty using adb (android debug bridge).
 
-
     it prepends folders to the name so the files are organized nicely on misty herself:
         - ./music/mario/super_mario_theme.mp3 -> music--mario--super_mario_theme.mp3
 
-    sample commands:
+    example commands:
         adb connect 192.168.86.24:5555
         adb push "./silence_stop.mp3" "/sdcard/audio/Audio/silence_stop.mp3"
         adb push "./misc/smooth_jazz_will_be_deployed.mp3" "/sdcard/audio/Audio/misc--smooth_jazz_will_be_deployed.mp3"
 
-
+    NOTE: you need to install adb (android debug bridge) on your system
     NOTE: reboot misty after the upload is complete so she can recognize the new files
     """
 
     os.chdir(d)
-    commands = [f'adb connect {os.environ["MISTY_IP"]}:5555']
+    commands = [f'adb connect {ip or os.environ["MISTY_IP"]}:{port}']
     for path, _, files in os.walk('.'):
         for f in files:
             local_fn = f'{path}/{f}'
