00c371a8d6a062206e0d998207d9405be5c37cfc
acushner <sweettuse@gmail.com>
2020-03-08 19:10:57 -0400

    added enable_module_properties

--- utils/core.py
+++ utils/core.py
@@ -1,9 +1,13 @@
 import os
 import pickle
 from collections import deque
+from functools import wraps
 from io import BytesIO
 from tempfile import NamedTemporaryFile
+from types import ModuleType
 from typing import Dict, Union, Tuple, List, Set, Iterable, Any
+import sys
+from inspect import isfunction
 
 __author__ = 'acushner'
 
@@ -41,6 +45,55 @@ def play_sound(file_or_path):
             f.write(file_or_path)
             return os.system(f'afplay {f.name}')
     return os.system(f'afplay {file_or_path}')
+
+
+def _wrap_property(prop):
+    """make it so property functions do not need `self` if defined at module level"""
+    new_props = {}
+
+    def _create_func(func):
+        return wraps(func)(lambda self, *args, **kwargs: func(*args, **kwargs))
+
+    for name in 'fget fset fdel'.split():
+        f = getattr(prop, name)
+        if f:
+            new_props[name] = _create_func(f)
+
+    return property(**new_props)
+
+
+def enable_module_properties():
+    """
+    allow properties to be used at the module level
+
+    to use, in the module, do something like:
+    >>> _b = 14
+    >>>
+    >>> @property
+    >>> def b():
+    >>>     return _b
+    >>>
+    >>> @b.setter
+    >>> def b(val)
+    >>>     global _b
+    >>>     _b = val
+    >>>
+    >>> if __name__ != '__main__':
+    >>>     enable_module_properties()
+    """
+    globs = sys._getframe(1).f_globals
+    name = globs['__name__']
+    mod = sys.modules[name]
+    props, body = {}, {}
+    for n, v in vars(mod).items():
+        if isinstance(v, property):
+            props[n] = _wrap_property(v)
+        else:
+            body[n] = v
+
+    new = sys.modules[name] = type(name, (type(mod),), props)(name)
+    for k, v in body.items():
+        setattr(new, k, v)
 
 
 def __main():
