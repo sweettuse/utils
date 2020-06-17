c2ed7d2f85c1f279b4ed4a070e72c56e7be4f5f5
acushner <sweettuse@gmail.com>
2020-06-16 22:33:31 -0400

    reqs

--- utils/scripts/days_since.sh
+++ utils/scripts/days_since.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-. /Users/acushner/scripts/ve_utils
-python3 /Users/acushner/software/sweettuse/utils/utils/slack_api/days_since.py
+. ~pi/scripts/ve_utils
+python3 ~pi/software/utils/utils/slack_api/days_since.py
