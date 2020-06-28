9e6c24d70072c0aa521964e5aa41bfa0fc755cd2
acushner <sweettuse@gmail.com>
2020-06-28 00:35:19 -0400

    change back to old way to figure out shelve metadata issues

--- utils/slack_api/days_since.py
+++ utils/slack_api/days_since.py
@@ -2,17 +2,17 @@ import asyncio
 import random
 from typing import List
 
-from utils.slack_api import UserType
+from utils.slack_api import UserType, async_run
 from utils.slack_api.api import SlackAPI
 from utils.web.incident import Incident, init_incident_store
 
 __author__ = 'acushner'
 
-# _incidents = [
-#     Incident('2020-05-18', 'an excel drag-down related incident'),
-#     Incident('2020-06-12', 'misinformation'),
-#     Incident('2020-06-22', 'a considerable brain fart'),
-# ]
+_incidents = [
+    Incident('2020-05-18', 'an excel drag-down related incident'),
+    Incident('2020-06-12', 'misinformation'),
+    Incident('2020-06-22', 'a considerable brain fart'),
+]
 
 
 async def _update_channel(sa: SlackAPI, channel_id, incidents: List[Incident], n=3):
@@ -21,15 +21,16 @@ async def _update_channel(sa: SlackAPI, channel_id, incidents: List[Incident], n
     strs = [i.with_emoji(await sa.random_emoji) for i in incidents[:n]]
     topic = "it's been " + ' '.join(strs)
     print(topic)
-    await sa.client.conversations_setTopic(channel=sa.channel_id(channel_id), topic=topic)
+    # await sa.client.conversations_setTopic(channel=sa.channel_id(channel_id), topic=topic)
 
 
 async def update_days_since():
     sa = await SlackAPI.from_user_type(UserType.bot)
-    incidents = init_incident_store().group_by_channel()
-    tasks = (asyncio.create_task(_update_channel(sa, channel, [ii.incident for ii in iis]))
-             for channel, iis in incidents.items())
-    await asyncio.gather(*tasks)
+    await _update_channel(sa, 'poc_crew', _incidents)
+    # incidents = init_incident_store().group_by_channel()
+    # tasks = (asyncio.create_task(_update_channel(sa, channel, [ii.incident for ii in iis]))
+    #          for channel, iis in incidents.items())
+    # await asyncio.gather(*tasks)
 
 
 async def run():
@@ -45,8 +46,8 @@ def fix_dates():
 
 def __main():
     # print(list(_get_days()))
-    # async_run(update_days_since())
-    fix_dates()
+    async_run(update_days_since())
+    # fix_dates()
 
 
 if __name__ == '__main__':
