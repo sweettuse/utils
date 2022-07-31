d568abec8844a491baee8244dd058e89b2721f50
acushner <sweettuse@gmail.com>
2022-07-31 02:12:12 -0400

    actually stack tracer

---
+++ utils/experimental/stack_tracer/stack_tracer.py
@@ -0,0 +1,111 @@
+from __future__ import annotations
+from contextlib import suppress
+import inspect
+from operator import itemgetter
+import sys
+from traceback import StackSummary
+from types import FrameType
+from typing import Any, Literal, Optional
+from typing_extensions import TypeAlias
+from rich import print
+
+from utils.experimental.mud.mud import run_by_clock
+
+EventType = Literal['call', 'line', 'return', 'exception', 'opcode']
+
+CodeStackKey: TypeAlias = tuple[str, str]
+CodeStackEntry: TypeAlias =tuple[CodeStackKey, inspect.Traceback] 
+CodeStackEntries: TypeAlias = list[CodeStackEntry]
+
+
+class CodeStack:
+    def __init__(self):
+        self._stack: CodeStackEntries = []
+        self._last_action: Optional[Literal['add', 'remove']] = None
+
+    def add(self, tb: inspect.Traceback) -> None:
+        self._stack.append((self._make_key(tb), tb))
+        self._last_action = 'add'
+
+    def remove(
+        self, tb: inspect.Traceback
+    ) -> Optional[CodeStackEntries]:
+        if not self._stack:
+            return
+
+        key = self._make_key(tb)
+        last_key, _ = self._stack[-1]
+        if key != last_key:
+            return
+
+        res = None
+        if self._last_action == 'add':
+            res = self._stack.copy()
+        self._last_action = 'remove'
+
+        self._stack.pop()
+        return res
+
+    @staticmethod
+    def _make_key(tb: inspect.Traceback) -> CodeStackKey:
+        return tb.filename, tb.function
+
+    def __bool__(self):
+        return bool(self._stack)
+
+    def __repr__(self) -> str:
+        res = ['=' * 20]
+        res.extend(map(itemgetter(0), self._stack))
+        res.append('=' * 20)
+        return '\n'.join(map(str, res))
+
+
+class StackTracer:
+    """context manager to trace stacks of calls within
+    
+    useful for debugging what code paths your code is taking when you can't easily set a breakpoint
+    """
+    def __init__(self, num_lines_context=0):
+        self._code_stack = CodeStack()
+        self._stacks = []
+        self._num_lines_context = num_lines_context
+        self._in_context = False  # are we within the context manager. don't want to display while within, can cause recursion issues...
+
+    def _local(self, frame: FrameType, event: EventType, arg: Any) -> None:
+        """local tracing function used to record return/exceptions"""
+        tb = inspect.getframeinfo(frame, context=self._num_lines_context)
+        if event in {'return', 'exception'}:
+            if res := self._code_stack.remove(tb):
+                self._stacks.append(res)
+
+    def __call__(self, frame: FrameType, event: EventType, arg: Any) -> None:
+        """actual global tracing function"""
+
+        # only trace functions in our codebase
+        if 'sweettuse/utils/utils' not in frame.f_code.co_filename:
+            return
+        tb = inspect.getframeinfo(frame, context=0)
+        self._code_stack.add(tb)
+
+        # set local tracing func
+        frame.f_trace = self._local
+        frame.f_trace_lines = False
+
+    def __enter__(self):
+        self._in_context = True
+        sys.settrace(self)
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self._in_context = False
+        sys.settrace(None)
+
+    def display(self) -> None:
+        if self._in_context:
+            return
+        print('=========')
+        for stack in self._stacks:
+            print(*(s[0][1] for s in stack))
+            # print(*map(itemgetter(0), stack))
+        print('=========')
+
---
+++ utils/experimental/stack_tracer/test.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+from contextlib import suppress
+from utils.experimental.stack_tracer.stack_tracer import CodeStackEntries, StackTracer
+
+
+# set up test functions to call
+def a():
+    b()
+    b()
+    c()
+
+
+def b():
+    d()
+    return 4
+
+
+def c():
+    ...
+
+
+def d():
+    ...
+
+
+def f():
+    g()
+
+
+def g():
+    h()
+
+
+def h():
+    raise Exception
+
+
+def _get_simple_stack(stack: CodeStackEntries) -> str:
+    """return pipe delimited stack of function names in a single str"""
+    return '|'.join(s[0][1] for s in stack)
+
+
+# test methods
+def _test_no_exception():
+    with StackTracer() as st:
+        a()
+    st.display()
+    assert 'a|b|d a|b|d a|c' == ' '.join(map(_get_simple_stack, st._stacks))
+
+
+def _test_exception():
+    with StackTracer() as st:
+        with suppress(Exception):
+            f()
+        d()
+        d()
+    st.display()
+    assert 'f|g|h d d' == ' '.join(map(_get_simple_stack, st._stacks))
+
+
+def _test_within():
+    with StackTracer() as st:
+        a()
+        st.display()
+
+
+# test
+_test_no_exception()
+_test_exception()
+_test_within()
